7e7b62f431a0f406c02a9edec54a9087ea34997d
DavideAlovisio13 <alovisiod@gmail.com>
2024-06-10 18:59:30 +0200

    cors change url front-end

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,6 @@ class ProjectController extends Controller
     public function index() {
         $projects = Project::all();
         if($projects) {
-            dd($projects);
             return response()->json([
                 'success' => true,
                 'results' => $projects
