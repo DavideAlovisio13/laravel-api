676f1bf61c7f6fc4fab643e6866a730e8ef577fa
DavideAlovisio13 <alovisiod@gmail.com>
2024-11-15 12:56:31 +0100

    change dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,20 +7,7 @@
     </h2>
     <div class="row justify-content-center">
         <div class="col">
-            <div class="card">
                 <a href="{{ route('admin.projects.index') }}"><h1>Projects</h1></a>
-                <div class="card-header">{{ __('User Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
         </div>
     </div>
 </div>
